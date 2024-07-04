f5b26c00f4504fad02b41b394b2fb503e0aa0c99
Maximilian Jugl <Maximilian.Jugl@medizin.uni-leipzig.de>
2024-07-04 09:58:48 +0200

    Add masking command with CLK filter

--- packages/pprl_client/pprl_client/cli.py
+++ packages/pprl_client/pprl_client/cli.py
@@ -6,12 +6,21 @@ from typing import Any, TypeVar, Type
 
 import click
 from pprl_model import MatchConfig, BitVectorEntity, MatchRequest, EntityTransformConfig, AttributeValueEntity, \
-    GlobalTransformerConfig, AttributeTransformerConfig, EntityTransformRequest
+    GlobalTransformerConfig, AttributeTransformerConfig, EntityTransformRequest, MaskConfig, CLKFilter, HashConfig, \
+    HashFunction, EntityMaskRequest
 from pydantic import BaseModel
 
 from pprl_client import lib
 
 _M = TypeVar("_M", bound=BaseModel)
+
+
+def _maybe_read_json(path: Path | None, encoding: str) -> Any | None:
+    if path is None:
+        return None
+
+    with open(path, mode="r", encoding=encoding) as f:
+        return json.load(f)
 
 
 def _parse_json_file_into(path: Path, model: Type[_M], encoding: str) -> _M:
@@ -24,6 +33,14 @@ def _maybe_parse_json_file_into(path: Path | None, model: Type[_M], encoding: st
         return None
 
     return _parse_json_file_into(path, model, encoding)
+
+
+def _maybe_parse_json_file_into_list_of(path: Path | None, model: Type[_M], encoding: str) -> list[_M] | None:
+    if path is None:
+        return None
+
+    with open(path, mode="r", encoding=encoding) as f:
+        return [model(**obj) for obj in json.load(f)]
 
 
 @click.group()
@@ -186,8 +203,8 @@ def transform(
     # noinspection PyTypeChecker
     config = EntityTransformConfig(empty_value=empty_value)
 
-    with open(entity_file_path, "r", encoding=encoding, newline="") as range_file:
-        reader = csv.DictReader(range_file, delimiter=delimiter)
+    with open(entity_file_path, "r", encoding=encoding, newline="") as entity_file:
+        reader = csv.DictReader(entity_file, delimiter=delimiter)
         csv_columns = reader.fieldnames
 
         if entity_id_column not in csv_columns:
@@ -209,11 +226,9 @@ def transform(
     global_config = (_maybe_parse_json_file_into(global_config_path, GlobalTransformerConfig, encoding)
                      or GlobalTransformerConfig())
 
-    if attribute_config_path is None:
-        attribute_config = []
-    else:
-        with open(attribute_config_path, "r", encoding=encoding) as f:
-            attribute_config = [AttributeTransformerConfig(**obj) for obj in json.load(f)]
+    attribute_config = _maybe_parse_json_file_into_list_of(
+        attribute_config_path, AttributeTransformerConfig, encoding
+    ) or []
 
     idx = list(range(0, len(entities), batch_size))
 
@@ -234,6 +249,115 @@ def transform(
                         entity_id_column: entity.id,
                         **entity.attributes
                     } for entity in transform_response.entities
+                ])
+
+
+@app.group()
+def mask():
+    pass
+
+
+@mask.command()
+@click.pass_context
+@click.argument("entity_file_path", type=click.Path(exists=True, path_type=Path))
+@click.argument("output_file_path", type=click.Path(dir_okay=False, file_okay=True, path_type=Path))
+@click.argument("filter_size", type=click.IntRange(min=1))
+@click.argument("hash_values", type=click.IntRange(min=1))
+@click.option("-q", "--token-size", type=click.IntRange(min=2), default=2)
+@click.option("--prepend-attribute-name/--no-prepend-attribute-name", default=True)
+@click.option("-p", "--padding", type=str, default="")
+@click.option("--hash-strategy", type=click.Choice(
+    ["double_hash", "enhanced_double_hash", "triple_hash", "random_hash"]
+), default="random_hash")
+@click.option("-h", "--hash-algorithm", type=click.Choice([
+    "md5", "sha1", "sha256", "sha512"
+]), multiple=True, default=["sha256"])
+@click.option("-s", "--hash-key", type=str)
+@click.option("--hardener-config-path", type=click.Path(exists=True, path_type=Path), default=None)
+@click.option("--attribute-config-path", type=click.Path(exists=True, path_type=Path), default=None)
+@click.option(
+    "--entity-id-column", type=str, default="id",
+    help="column name in entity CSV file containing ID"
+)
+def clk(
+        ctx: click.Context,
+        entity_file_path: Path,
+        output_file_path: Path,
+        filter_size: int,
+        hash_values: int,
+        token_size: int,
+        prepend_attribute_name: bool,
+        padding: str,
+        hash_strategy: str,
+        hash_algorithm: list[str],
+        hash_key: str,
+        hardener_config_path: Path | None,
+        attribute_config_path: Path | None,
+        entity_id_column: str,
+):
+    base_url = ctx.obj["BASE_URL"]
+    batch_size = ctx.obj["BATCH_SIZE"]
+    timeout_secs = ctx.obj["TIMEOUT_SECS"]
+    delimiter = ctx.obj["DELIMITER"]
+    encoding = ctx.obj["ENCODING"]
+
+    hardener_json = _maybe_read_json(hardener_config_path, encoding) or []
+
+    # noinspection PyTypeChecker
+    mask_config = MaskConfig(
+        token_size=token_size,
+        hash=HashConfig(
+            function=HashFunction(
+                algorithms=hash_algorithm,
+                key=hash_key
+            ),
+            strategy={"name": hash_strategy}
+        ),
+        prepend_attribute_name=prepend_attribute_name,
+        filter=CLKFilter(filter_size=filter_size, hash_values=hash_values),
+        padding=padding,
+        hardeners=hardener_json,
+    )
+
+    with open(entity_file_path, "r", encoding=encoding, newline="") as entity_file:
+        reader = csv.DictReader(entity_file, delimiter=delimiter)
+
+        if entity_id_column not in reader.fieldnames:
+            click.echo(f"Column {entity_id_column} not found in CSV file", err=True)
+            ctx.exit(1)
+
+        def _row_to_entity(row: dict[str, Any]) -> AttributeValueEntity:
+            return AttributeValueEntity(
+                id=str(row[entity_id_column]),
+                attributes={
+                    # exclude ID column from attribute set
+                    k: str(v) for k, v in row.items() if k != entity_id_column
+                }
+            )
+
+        # noinspection PyTypeChecker
+        entities = [_row_to_entity(row) for row in reader]
+
+    attribute_json = _maybe_read_json(attribute_config_path, encoding) or []
+    idx = list(range(0, len(entities), batch_size))
+
+    with open(output_file_path, mode="w", encoding=encoding, newline="") as output_file:
+        writer = csv.DictWriter(output_file, delimiter=delimiter, fieldnames=[entity_id_column, "value"])
+        writer.writeheader()
+
+        with click.progressbar(idx, label="Masking entities") as progressbar:
+            for i in progressbar:
+                mask_response = lib.mask(EntityMaskRequest(
+                    config=mask_config,
+                    entities=entities[i:i + batch_size],
+                    attributes=attribute_json,
+                ), base_url=base_url, timeout_secs=timeout_secs)
+
+                writer.writerows([
+                    {
+                        entity_id_column: entity.id,
+                        "value": entity.value
+                    } for entity in mask_response.entities
                 ])
 
 
